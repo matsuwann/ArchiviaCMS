e1484768a63601eff2a99b5adc3b78b3b4b3fda6
Roswellgit <mimicry4us@gmail.com>
2025-11-13 15:08:27 +0800

    Fix fileupload

--- Backend/services/fileUploadService.js
+++ Backend/services/fileUploadService.js
@@ -24,19 +24,8 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const dest = 'uploads/';
-    ensureDir(dest, (err) => cb(err, dest));
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-  }
-});
-
 exports.upload = multer({ 
-  storage: storage,
+  storage: multer.memoryStorage(),
   fileFilter: fileFilter
 });
 
